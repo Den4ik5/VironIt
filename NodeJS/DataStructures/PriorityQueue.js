c5619a6869b6a497e624fc2719c0084ad8500a96
Den4ik5 <polit.den63@gmail.com>
2019-09-04 15:27:08 +0300

    Added Priority Queue

--- NodeJS/DataStructures/PriorityQueue.js
+++ NodeJS/DataStructures/PriorityQueue.js
@@ -1,11 +1,68 @@
+'use strict';
 class PriorityQueue {
+
     constructor(){
-        this.queue = {};
-    }
-    push(){
+        this.storage = new Map();
+        this.priorities=[];
 
     }
-    pop(){
 
+    push(priority, value){
+        this.storage.set(priority, value);
+        this.priorities.push(priority);
+    }
+
+    pop(key){
+        this.storage.delete(key);
+    }
+    removePriority(value){
+        this.priorities.forEach(function (item, id, array) {
+            if(item===value){
+                array.splice(id, 1);
+            }
+        })
+    }
+
+    findMinimalPriority(){
+        let min = this.priorities[0];
+        this.priorities.forEach(function (item) {
+            min = item < min ? item : min;
+        });
+        return min;
+    }
+
+    findMaximalPriority(){
+        let max = this.priorities[0];
+        this.priorities.forEach(function (item) {
+            max = item > max ? item : max;
+        });
+        return max;
+    }
+    extractMinimum() {
+        let min = this.findMinimalPriority();
+        while(this.storage.has(min)){
+            console.log(this.storage.get(min));
+            this.pop(min);
+        }
+        this.removePriority(min);
+
+    }
+    extractMaximum(){
+        let max = this.findMaximalPriority();
+        while (this.storage.has(max)) {
+            console.log(this.storage.get(max));
+            this.pop(max);
+        }
+        this.removePriority(max);
     }
 }
+
+
+let prior = new PriorityQueue();
+prior.push(1, 4);
+prior.push(3, 4);
+prior.push(2, 4);
+prior.extractMinimum();
+prior.extractMaximum();
+console.log(prior.priorities.length);
+
