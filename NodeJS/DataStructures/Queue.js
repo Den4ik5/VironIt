333a262ae5bfc851972da5312f3a6423bbad8da0
Den4ik5 <polit.den63@gmail.com>
2019-09-04 13:07:39 +0300

    project structure Fixed

--- NodeJS/DataStructures/Queue.js
+++ NodeJS/DataStructures/Queue.js
@@ -1,24 +1,4 @@
-//Stack = require('./Stack');
-
-
-class Stack {
-    constructor(){
-        this.stack = [];
-    }
-    push(el){
-        this.stack.push(el);
-    }
-    pop(){
-        let el = this.stack.pop();
-        return el;
-    }
-    size(){
-        return this.stack.length;
-    }
-    viewFirst(){
-        return this.stack[this.stack.length-1];
-    }
-}
+Stack = require('./Stack');
 
 class Queue extends Stack{
     constructor(){
