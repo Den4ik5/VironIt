ea01c76f1299efd9c2757428cbfcb625d5291396
Den4ik5 <polit.den63@gmail.com>
2019-10-14 13:10:52 +0300

    tested encryption succesfully

--- NodeJS/test encryption/index.js
+++ NodeJS/test encryption/index.js
@@ -1,7 +1,24 @@
 const password = '123123';
+const userId = 'jg3kj1ggyuugkyuhe872y3j2k`123';
+const passPhrase = userId;
 const cryptico = require('cryptico');
 const bits = 1024;
-const RSAKey= cryptico.generateRSAKey(password, bits);
+const RSAKey= cryptico.generateRSAKey(passPhrase, bits);
+const RSAKEY2 = cryptico.generateRSAKey(passPhrase, bits);
+const publicKey2 = cryptico.publicKeyString(RSAKEY2);
+const publicKey = cryptico.publicKeyString(RSAKey);
 
+const encryptionResult = cryptico.encrypt(password, publicKey);
 
-console.log(RSAKey);
+const decryptionResult = cryptico.decrypt(encryptionResult.cipher, RSAKey);
+
+console.log(publicKey);
+console.log('------');
+console.log(encryptionResult);
+console.log('--------');
+console.log(encryptionResult.cipher);
+console.log('------');
+console.log(decryptionResult.plaintext);
+console.log('-----');
+console.log(publicKey === publicKey2);
+
